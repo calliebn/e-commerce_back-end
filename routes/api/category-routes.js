0e53b27fe6564383960f8761e6de02f7b82cd763
Callie Nipper <callie.nipper@gmail.com>
2021-04-21 16:17:30 -0700

    Created get Category by ID route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,6 +24,26 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then(dbCatData => {
+    if (!dbCatData) {
+    res.status(404).json({ 'No categories found' });
+    return;
+    }
+    res.json(dbCatData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
   // find one category by its `id` value
   // be sure to include its associated Products
 });
